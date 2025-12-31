1f703f65a4bf80fd51a0b0b7fbe9bf09cf1bda7b
Ashish Yadav <ashish@Ashishs-MacBook-Air.local>
2025-12-31 19:12:11 +0530

    Maintained clean code structure and indentation

---
+++ level1_practiceproblem/AgeCalculator.java
@@ -0,0 +1,15 @@
+package level1_practiceproblem;
+
+public class AgeCalculator {
+
+    public static void main(String[] args) {
+
+        String name = "Harry";
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println(name + "'s age in " + currentYear + " is " + age);
+    }
+}
