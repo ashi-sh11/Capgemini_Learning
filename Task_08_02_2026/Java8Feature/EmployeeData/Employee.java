f2e52d1b87fd2be7f15521cc7a4737400f4cd177
Ashish Yadav <ashish@Ashishs-MacBook-Air.local>
2026-02-08 23:03:51 +0530

    Adding Question of Java 8 Features

---
+++ Task_08_02_2026/Java8Feature/EmployeeData/Employee.java
@@ -0,0 +1,28 @@
+package Task_08_02_2026.Java8Feature.EmployeeData;
+
+class Employee {
+    int id;
+    String name;
+    String department;
+    double salary;
+
+    Employee(int id, String name, String department, double salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + salary;
+    }
+}
